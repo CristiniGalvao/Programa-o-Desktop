59455c65825b2240c82024e7133612deefc528b8
Gabriella Cristini <126834537+CristiniGalvao@users.noreply.github.com>
2024-03-06 21:42:56 -0300

    Add files via upload

---
+++ Exer1/src/main/java/com/mycompany/exer1/Exer1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exer1;
+
+/**
+ *
+ * @author aluno
+ */
+public class Exer1 {
+
+    public static void main(String[] args) {
+       new FrameExer1().setVisible(true);
+    }
+}
